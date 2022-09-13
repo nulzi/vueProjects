c6ce1c1c4fa21b0e947738591e75a8e38845aa13
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-09-13 12:40:11 +0900

    [E] metatft
    Modal이 화면을 넘어가는 오류 발생
    https://whitesal.com/article/article_view.php?no=2702&mode=3
    해결

--- metatft/src/components/ModalPage.vue
+++ metatft/src/components/ModalPage.vue
@@ -20,6 +20,7 @@ export default {
 
 <style scoped>
 .black-bg {
+  box-sizing: border-box;
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
@@ -27,6 +28,7 @@ export default {
   padding: 60px;
 }
 .white-bg {
+  box-sizing: border-box;
   width: 100%;
   background: white;
   border-radius: 8px;
