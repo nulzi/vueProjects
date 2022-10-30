ad416271577df1112f2f9ca5ce1b263177f70de4
nulzi <kimty131@naver.com>
2022-10-30 12:03:40 +0900

    [U] lolchess Filter
    UnitCostFilter() end
    UnitTraitsFilter() ~ing

--- lolchess/src/components/Filter/UnitFilterContainer.vue
+++ lolchess/src/components/Filter/UnitFilterContainer.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- {{ this.traitsFilter() }} -->
+  {{ this.costFilter() }}
   <div class="filter-container">
     <Header @reset="reset"></Header>
     <Content :content="content" @content="changeContent"></Content>
@@ -13,6 +15,8 @@ import Content from './ContentFilter.vue';
 import UnitCost from './UnitCostFilter.vue';
 import UnitTraits from './UnitTraitsFilter.vue';
 
+import newdata from '../../assets/newdata.json';
+
 export default {
   props: ['content', 'cost', 'traits'],
   components: {
@@ -22,7 +26,7 @@ export default {
     UnitTraits,
   },
   data() {
-    return {};
+    return { newdata };
   },
   methods: {
     reset() {
@@ -40,6 +44,52 @@ export default {
     changeTrait(traits) {
       // console.log(`fileter: ${traits}`);
       this.$emit('traits', traits);
+    },
+    costFilter() {
+      const { champions } = this.newdata.setData[0];
+      const costs = [1, 0, 1, 1, 0];
+      let champs = [];
+      for (let cost = 0; cost < costs.length; cost++) {
+        if (costs[cost] == 1) {
+          const temp = champions.filter((v) => v.cost == cost + 1);
+          // console.log(temp);
+          champs.push(temp);
+        }
+      }
+      let temp = champs[0];
+      if (champs.length > 1) {
+        for (let i = 1; i < champs.length; i++) {
+          temp = temp.concat(champs[i]);
+        }
+      }
+      champs = temp;
+      console.log(champs);
+    },
+    traitsFilter() {
+      const { champions } = this.newdata.setData[0];
+      const filteredChampionsTrait = champions.filter((v) =>
+        v.traits.includes('Astral')
+      );
+      console.log(filteredChampionsTrait);
+    },
+    testFilter() {
+      const objPersons = {
+        persons: [
+          { name: 'kim', age: 20, gender: 'man' },
+          { name: 'vicky', age: 22, gender: 'women' },
+          { name: 'gloria', age: 42, gender: 'women' },
+          { name: 'gom', age: 27, gender: 'man' },
+        ],
+      };
+      const { persons } = objPersons;
+      // ES5: const persons = Object.assign([], objPersons.persons)와 동일
+      console.log(Object.values(persons));
+      const mapPersons = Object.values(persons).map((v) =>
+        Object.keys(v).map((v2) => v[v2])
+      );
+      const filteredPersonsAge = persons.filter((v) => v.age > 25);
+      console.log(mapPersons);
+      console.log(filteredPersonsAge);
     },
   },
 };
