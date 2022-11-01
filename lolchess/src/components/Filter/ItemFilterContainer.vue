fbcbc00a9eeee638dc125632d2d520fb37046d23
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-11-02 01:48:47 +0900

    [U] lolchess Item Filter Container
    test filter function

--- lolchess/src/components/Filter/ItemFilterContainer.vue
+++ lolchess/src/components/Filter/ItemFilterContainer.vue
@@ -1,4 +1,10 @@
 <template>
+  <!-- {{ typeof '' }} -->
+  <!-- {{ this.typeFilter().length }}
+  <div v-for="id in this.typeFilter()" :key="id">
+    {{ id }}
+    <img :src="GetItemUrl(id)" alt="" />
+  </div> -->
   <div class="filter-container">
     <Header @reset="reset"></Header>
     <Content :content="content" @content="changeContent"></Content>
@@ -14,6 +20,8 @@ import Content from './ContentFilter.vue';
 import Type from './ItemTypeFilter.vue';
 import Base from './BaseItemFilter.vue';
 
+import newdata from '../../assets/newdata.json';
+
 export default {
   props: ['content', 'type', 'base'],
   components: {
@@ -23,7 +31,7 @@ export default {
     Base,
   },
   data() {
-    return {};
+    return { newdata };
   },
   methods: {
     reset() {
@@ -41,6 +49,64 @@ export default {
     changeBase(itemID) {
       // console.log(`fileter: ${itemID}`);
       this.$emit('base', itemID);
+    },
+    baseFilter() {
+      const itemID = 1;
+      let itemArray = [];
+      for (let i in this.newdata.items) {
+        if (
+          this.newdata.items[i].from.includes(itemID) ||
+          this.newdata.items[i].id == itemID
+        ) {
+          const temp = this.newdata.items[i].id;
+          // console.log(temp);
+          itemArray.push(temp);
+        }
+      }
+      console.log(itemArray);
+      return itemArray;
+    },
+    GetItemUrl(item) {
+      for (let j in newdata.items) {
+        if (item == newdata.items[j].id) {
+          // console.log(newdata.items[j].icon.toLowerCase().split('.'));
+          let temp = newdata.items[j].icon
+            .toLowerCase()
+            .split('.')
+            .slice(0, -1);
+          return `https://raw.communitydragon.org/latest/game/${temp.join(
+            '.'
+          )}.png`;
+        }
+      }
+    },
+    typeFilter() {
+      let itemArray = [];
+      for (let i in this.newdata.items) {
+        // if (
+        //   !this.newdata.items[i].apiName.includes('Augment') &&
+        //   this.newdata.items[i].apiName.includes('Emblem')
+        // ) {
+        //   const temp = this.newdata.items[i].id;
+        //   console.log(temp);
+        //   itemArray.push(temp);
+        // }
+        const word = 'Shimmerscale/';
+        // Ornn
+        // Radiant/
+        // Standard/ need exception
+        // Shimmerscale/ duplicate id 3000, 3010
+        // Emblem
+        // console.log(this.newdata.items[i].name.includes(word));
+        // if (this.newdata.items[i].name === '') continue;
+        // else
+        if (this.newdata.items[i].icon.includes(word)) {
+          const temp = this.newdata.items[i].id;
+          console.log(temp);
+          itemArray.push(temp);
+        }
+      }
+      return itemArray;
     },
   },
 };
