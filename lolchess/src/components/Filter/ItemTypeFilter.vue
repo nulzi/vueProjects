bdffe57e11777196195b3dcd2d54e6065db88bdf
nulzi <kimty131@naver.com>
2022-11-08 21:24:18 +0900

    [U] lolchess typefilter
    useless code 주석 처리

--- lolchess/src/components/Filter/ItemTypeFilter.vue
+++ lolchess/src/components/Filter/ItemTypeFilter.vue
@@ -57,27 +57,28 @@ export default {
       return temp;
     },
     changeType(type, index) {
+      console.log('changeType()');
       const classList = document.getElementById(type).classList;
-      const word = [
-        'Standard/',
-        'Emblem',
-        'Ornn_',
-        'Radiant/',
-        'Shimmerscale/',
-      ];
+      // const word = [
+      //   'Standard/',
+      //   'Emblem',
+      //   'Ornn_',
+      //   'Radiant/',
+      //   'Shimmerscale/',
+      // ];
       // filter off
       if (classList.contains('clicked')) {
         this.isClicked[index] = 0;
-        this.filter = this.filter.filter((type) => type != word[index]);
+        // this.filter = this.filter.filter((type) => type != word[index]);
         classList.replace('clicked', 'unclicked');
       } else {
         //filter on
         this.isClicked[index] = 1;
-        this.filter.push(word[index]);
+        // this.filter.push(word[index]);
         classList.replace('unclicked', 'clicked');
       }
-      this.filteredItems = this.typesFilter(this.filter);
-      this.excute();
+      // this.filteredItems = this.typesFilter(this.filter);
+      // this.excute();
       this.$emit('type', index);
     },
     typesFilter(filter) {
