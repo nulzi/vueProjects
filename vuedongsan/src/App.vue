07ec4259034bf2c0a19592d3e27c020ceec71de0
nulzi <kimty131@naver.com>
2023-02-26 18:16:15 +0900

    #vuedongsan
    event listener & methods

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -2,9 +2,23 @@
   <div class="menu">
     <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
-  <div v-for="product in products" :key="product">
-    <h4>{{ product }}</h4>
+  <div>
+    <h4>{{ products[0] }}</h4>
     <p>60 만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[0] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>60 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[1] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>60 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[2] }}</span>
   </div>
 </template>
 
@@ -15,7 +29,13 @@ export default {
     return {
       menus: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      reportNum: [0, 0, 0],
     };
+  },
+  methods: {
+    increase(index) {
+      this.reportNum[index]++;
+    },
   },
   components: {},
 };
