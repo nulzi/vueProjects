985414faeff7fd46b218b96fd007d5478bd604ab
nulzi <kimty131@naver.com>
2023-03-02 14:37:21 +0900

    # blog  복습
    databinding, props

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,17 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">TEST</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>React 개발자의 블로그입니다.</h5>
+    <p>- Vue로 만들었음 -</p>
+  </div>
+
+  <List :blogList="blogList" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from "./components/List.vue";
+import blog from "./assets/blog.js";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      blogList: blog,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    List: List,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +64,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="blog in blogList" :key="blog.number">
+    <h5>{{ blog.title }}</h5>
+    <p>{{ blog.date }}</p>
+    <p>{{ blog.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "List",
+  props: { blogList: Array },
+};
+</script>
+
+<style></style>
