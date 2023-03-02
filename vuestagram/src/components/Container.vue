a64527e90eebda7529333a45d44c32c1348f979d
nulzi <kimty131@naver.com>
2023-03-02 15:47:34 +0900

    #vuestagram
    layout & style property databinding

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post v-for="(post, i) in postList" :key="i" :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "container",
+  props: {
+    postList: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
